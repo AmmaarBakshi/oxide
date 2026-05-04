1cf7116f543406f86a088f78d7728eaab43903c2
AMMAAR-IC <the.ammaar.ic@gmail.com>
2026-05-05 02:16:14 +0530

    update: help command (1)

--- crates/oxide-builtins/src/help.rs
+++ crates/oxide-builtins/src/help.rs
@@ -1,7 +1,12 @@
 pub fn execute(args: &[String]) -> i32 {
     if args.is_empty() {
         println!("Available built-in commands:");
-        println!("alias cat cd clear echo env export find grep help history jail kill ls mode open pwd ps rm sleep source top touch unset");
+        
+        // One string, split by space, printed line by line
+        "alias cat cd clear echo env export find grep help history jail kill ls mode open pwd ps rm sleep source top touch unset"
+            .split_whitespace()
+            .for_each(|cmd| println!("{}", cmd));
+            
         return 0;
     }
 
